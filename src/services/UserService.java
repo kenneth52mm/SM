2dc5f871b04ae8c6364a07575ca2ff45686b4530
kenneth52mm <kenneth52mm@hotmail.com>
2015-06-16 18:01:21 -0600

    test

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -1,8 +1,18 @@
 package services;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/userService")
 public class UserService {
+
+	@GET
+	@Path("/test")
+	@Produces(MediaType.TEXT_XML) 
+	public String test() {
+		return "<name>kenneth</name>";
+	}
 
 }
